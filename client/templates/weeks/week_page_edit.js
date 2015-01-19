0a2cef8ee003aed83ad7d33b718cd8348c3e01b0
sushiogoto <yoshio.goto@gmail.com>
2015-01-19 14:09:48 +0800

    Add initial worldbank json callback functionality and connect it to client side

--- client/templates/weeks/week_page_edit.js
+++ client/templates/weeks/week_page_edit.js
@@ -5,5 +5,10 @@ Template.weekPage.events({
       journal: $(event.target).find('[name=journal]').val(),
     };
     Meteor.call('weekUpdate', weekDetails, this.date, function (error, result) {});
+  },
+  'blur .dropdown': function(event) {
+    Meteor.call('checkWorldBank', "MA", "HK", 1989, function (error, result) {
+      console.log(JSON.parse(result.content)[1]["0"].value);
+    });
   }
 });
